c10bd2c814e23c321fe62249ff52eeb9e1293c39
Sascha Falk <sascha@falk-online.eu>
2019-01-28 17:32:21 +0100

    Added unit tests for the ChainableMemoryBlock class.

--- src/GriffinPlus.Lib.Common.Tests/GriffinPlus.Lib.Io/ChainableMemoryBlockTests.cs
+++ src/GriffinPlus.Lib.Common.Tests/GriffinPlus.Lib.Io/ChainableMemoryBlockTests.cs
@@ -11,16 +11,95 @@
 // the specific language governing permissions and limitations under the License.
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-using GriffinPlus.Lib.Io;
+using System;
 using Xunit;
 
-namespace UnitTests
+namespace GriffinPlus.Lib.Io
 {
 	/// <summary>
 	/// Unit tests targetting the <see cref="ChainableMemoryBlock"/> class.
 	/// </summary>
 	public class ChainableMemoryBlockTests
 	{
-		// TODO
+		/// <summary>
+		/// Checks the creation of a memory block.
+		/// </summary>
+		/// <param name="capacity">Capacity of the memory block to create.</param>
+		[Theory]
+		[InlineData(0)]
+		[InlineData(1)]
+		// [InlineData(int.MaxValue)] // will cause problems on build servers with low memory
+		public void Create(int capacity)
+		{
+			ChainableMemoryBlock block = new ChainableMemoryBlock(capacity);
+			Assert.Equal(capacity, block.Capacity);
+			Assert.NotNull(block.Buffer);
+			Assert.Equal(capacity, block.Buffer.Length);
+			Assert.Equal(0, block.Length);
+			Assert.Null(block.Next);
+		}
+
+		/// <summary>
+		/// Checks setting the effective length of the memory block.
+		/// </summary>
+		/// <param name="valid">
+		/// true, if setting the length is expected to succeed;
+		/// false, if setting the length is expected to fail.
+		/// </param>
+		/// <param name="capacity">Capacity of the memory block to test.</param>
+		/// <param name="length">Length to try to set on the memory block.</param>
+		[Theory]
+		[InlineData(true,    0,    0)]
+		[InlineData(true,  100,    0)]
+		[InlineData(true,  100,    1)]
+		[InlineData(true,  100,   99)]
+		[InlineData(true,  100,  100)]
+		[InlineData(false,   0,   -1)]
+		[InlineData(false,   0,    1)]
+		[InlineData(false, 100,   -1)]
+		[InlineData(false, 100,  101)]
+		public void Length(bool valid, int capacity, int length)
+		{
+			ChainableMemoryBlock block1 = new ChainableMemoryBlock(capacity);
+			if (valid)
+			{
+				// setting is expected to succeed
+				block1.Length = length;
+				Assert.Equal(length, block1.Length);
+			}
+			else
+			{
+				// setting is expected to fail
+				ArgumentOutOfRangeException ex = Assert.Throws<ArgumentOutOfRangeException>(() => {
+					block1.Length = length;
+				});
+
+				Assert.Equal("value", ex.ParamName);
+			}
+		}
+
+		/// <summary>
+		/// Chains two memory blocks and determines the total length.
+		/// </summary>
+		[Fact]
+		public void ChainLength()
+		{
+			// create the first block
+			ChainableMemoryBlock block1 = new ChainableMemoryBlock(100);
+			block1.Length = 10;
+			Assert.Equal(10, block1.ChainLength);
+
+			// create the second block
+			ChainableMemoryBlock block2 = new ChainableMemoryBlock(200);
+			block2.Length = 20;
+			Assert.Equal(20, block2.ChainLength);
+
+			// chain both blocks together
+			block1.Next = block2;
+			Assert.Equal(30, block1.ChainLength);
+			Assert.Equal(20, block2.ChainLength);
+		}
+
+
 	}
 }
