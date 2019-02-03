e2dc8ef4dd74ad00e453fab836ce1fc021a09409
Sascha Falk <sascha@falk-online.eu>
2019-02-03 21:46:57 +0100

    Made unit tests use the integrated version of Stephen Cleary's AsyncEx library.

--- src/GriffinPlus.Lib.Common.Tests/GriffinPlus.Lib.Events/EventManagerTests.cs
+++ src/GriffinPlus.Lib.Common.Tests/GriffinPlus.Lib.Events/EventManagerTests.cs
@@ -11,7 +11,7 @@
 // the specific language governing permissions and limitations under the License.
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-using Nito.AsyncEx;
+using GriffinPlus.Lib.Threading;
 using System;
 using System.Linq;
 using System.Threading;
