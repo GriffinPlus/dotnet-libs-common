e1ae4bbc718000c0dd3da05181885c7909238a34
Sascha Falk <sascha@falk-online.eu>
2019-04-14 18:43:08 +0200

    Integrated Stephen Cleary's AsyncMonitor class.

---
+++ src/GriffinPlus.Lib.Common.Tests/GriffinPlus.Lib.Threading/Task Parallel Library (TPL)/Coordination/AsyncMonitorTests.cs
@@ -0,0 +1,170 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file is part of the Griffin+ common library suite (https://github.com/GriffinPlus/dotnet-libs-common)
+//
+// Copyright 2019 Sascha Falk <sascha@falk-online.eu>
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+// the specific language governing permissions and limitations under the License.
+//
+// This file incorporates work covered by the following copyright and permission notice:
+//
+//     MIT License
+//
+//     Copyright (c) 2019 Stephen Cleary
+//
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//
+//     The above copyright notice and this permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable file AccessToModifiedClosure
+
+namespace GriffinPlus.Lib.Threading
+{
+	public class AsyncMonitorUnitTests
+	{
+		[Fact]
+		public async Task Unlocked_PermitsLock()
+		{
+			var monitor = new AsyncMonitor();
+
+			var task = monitor.EnterAsync();
+			await task;
+		}
+
+		[Fact]
+		public async Task Locked_PreventsLockUntilUnlocked()
+		{
+			var monitor = new AsyncMonitor();
+			var task1HasLock = TaskCompletionSourceExtensions.CreateAsyncTaskSource<object>();
+			var task1Continue = TaskCompletionSourceExtensions.CreateAsyncTaskSource<object>();
+
+			var unused = Task.Run(async () =>
+			{
+				using (await monitor.EnterAsync())
+				{
+					task1HasLock.SetResult(null);
+					await task1Continue.Task;
+				}
+			});
+			await task1HasLock.Task;
+
+			var lockTask = monitor.EnterAsync().AsTask();
+			Assert.False(lockTask.IsCompleted);
+			task1Continue.SetResult(null);
+			await lockTask;
+		}
+
+		[Fact]
+		public async Task Pulse_ReleasesOneWaiter()
+		{
+			var monitor = new AsyncMonitor();
+			int completed = 0;
+			var task1Ready = TaskCompletionSourceExtensions.CreateAsyncTaskSource<object>();
+			var task2Ready = TaskCompletionSourceExtensions.CreateAsyncTaskSource<object>();
+			var task1 = Task.Run(async () =>
+			{
+				using (await monitor.EnterAsync())
+				{
+					var waitTask1 = monitor.WaitAsync();
+					task1Ready.SetResult(null);
+					await waitTask1;
+					Interlocked.Increment(ref completed);
+				}
+			});
+			await task1Ready.Task;
+			var task2 = Task.Run(async () =>
+			{
+				using (await monitor.EnterAsync())
+				{
+					var waitTask2 = monitor.WaitAsync();
+					task2Ready.SetResult(null);
+					await waitTask2;
+					Interlocked.Increment(ref completed);
+				}
+			});
+			await task2Ready.Task;
+
+			using (await monitor.EnterAsync())
+			{
+				monitor.Pulse();
+			}
+			await Task.WhenAny(task1, task2);
+			var result = Interlocked.CompareExchange(ref completed, 0, 0);
+
+			Assert.Equal(1, result);
+		}
+
+		[Fact]
+		public async Task PulseAll_ReleasesAllWaiters()
+		{
+			var monitor = new AsyncMonitor();
+			int completed = 0;
+			var task1Ready = TaskCompletionSourceExtensions.CreateAsyncTaskSource<object>();
+			var task2Ready = TaskCompletionSourceExtensions.CreateAsyncTaskSource<object>();
+			Task waitTask1;
+			var task1 = Task.Run(async () =>
+			{
+				using (await monitor.EnterAsync())
+				{
+					waitTask1 = monitor.WaitAsync();
+					task1Ready.SetResult(null);
+					await waitTask1;
+					Interlocked.Increment(ref completed);
+				}
+			});
+			await task1Ready.Task;
+			Task waitTask2;
+			var task2 = Task.Run(async () =>
+			{
+				using (await monitor.EnterAsync())
+				{
+					waitTask2 = monitor.WaitAsync();
+					task2Ready.SetResult(null);
+					await waitTask2;
+					Interlocked.Increment(ref completed);
+				}
+			});
+			await task2Ready.Task;
+
+			var lockTask3 = monitor.EnterAsync();
+			using (await lockTask3)
+			{
+				monitor.PulseAll();
+			}
+			await Task.WhenAll(task1, task2);
+			var result = Interlocked.CompareExchange(ref completed, 0, 0);
+
+			Assert.Equal(2, result);
+		}
+
+		[Fact]
+		public void Id_IsNotZero()
+		{
+			var monitor = new AsyncMonitor();
+			Assert.NotEqual(0, monitor.Id);
+		}
+	}
+}
---
+++ src/GriffinPlus.Lib.Common/GriffinPlus.Lib.Threading/Task Parallel Library (TPL)/Coordination/AsyncMonitor.cs
@@ -0,0 +1,171 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file is part of the Griffin+ common library suite (https://github.com/GriffinPlus/dotnet-libs-common)
+//
+// Copyright 2019 Sascha Falk <sascha@falk-online.eu>
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+// the specific language governing permissions and limitations under the License.
+//
+// This file incorporates work covered by the following copyright and permission notice:
+//
+//     MIT License
+//
+//     Copyright (c) 2019 Stephen Cleary
+//
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//
+//     The above copyright notice and this permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GriffinPlus.Lib.Threading
+{
+	/// <summary>
+	/// An async-compatible monitor.
+	/// Note that the monitor is <b>not</b> recursive!
+	/// </summary>
+	[DebuggerDisplay("Id = {Id}, ConditionVariableId = {mConditionVariable.Id}")]
+	public sealed class AsyncMonitor
+	{
+		/// <summary>
+		/// The lock.
+		/// </summary>
+		private readonly AsyncLock mAsyncLock;
+
+		/// <summary>
+		/// The condition variable.
+		/// </summary>
+		private readonly AsyncConditionVariable mConditionVariable;
+
+		/// <summary>
+		/// Creates a new monitor.
+		/// </summary>
+		/// <param name="lockQueue">
+		/// The wait queue used to manage waiters for the lock.
+		/// This may be <c>null</c> to use a default (FIFO) queue.
+		/// </param>
+		/// <param name="conditionVariableQueue">
+		/// The wait queue used to manage waiters for the signal.
+		/// This may be <c>null</c> to use a default (FIFO) queue.
+		/// </param>
+		internal AsyncMonitor(IAsyncWaitQueue<IDisposable> lockQueue, IAsyncWaitQueue<object> conditionVariableQueue)
+		{
+			mAsyncLock = new AsyncLock(lockQueue);
+			mConditionVariable = new AsyncConditionVariable(mAsyncLock, conditionVariableQueue);
+		}
+
+		/// <summary>
+		/// Creates a new monitor.
+		/// </summary>
+		public AsyncMonitor()
+			: this(null, null)
+		{
+		}
+
+		/// <summary>
+		/// Gets a semi-unique identifier for this monitor.
+		/// </summary>
+		public int Id => mAsyncLock.Id;
+
+		/// <summary>
+		/// Asynchronously enters the monitor.
+		/// Returns a disposable that leaves the monitor when disposed.
+		/// </summary>
+		/// <param name="cancellationToken">
+		/// The cancellation token used to cancel the enter.
+		/// If this is already set, then this method will attempt to enter the monitor immediately (succeeding if the monitor is currently available).
+		/// </param>
+		/// <returns>A disposable that leaves the monitor when disposed.</returns>
+		public AwaitableDisposable<IDisposable> EnterAsync(CancellationToken cancellationToken)
+		{
+			return mAsyncLock.LockAsync(cancellationToken);
+		}
+
+		/// <summary>
+		/// Asynchronously enters the monitor.
+		/// Returns a disposable that leaves the monitor when disposed.
+		/// </summary>
+		/// <returns>A disposable that leaves the monitor when disposed.</returns>
+		public AwaitableDisposable<IDisposable> EnterAsync()
+		{
+			return EnterAsync(CancellationToken.None);
+		}
+
+		/// <summary>
+		/// Synchronously enters the monitor.
+		/// Returns a disposable that leaves the monitor when disposed.
+		/// This method may block the calling thread.
+		/// </summary>
+		/// <param name="cancellationToken">
+		/// The cancellation token used to cancel the enter.
+		/// If this is already set, then this method will attempt to enter the monitor immediately (succeeding if the monitor is currently available).
+		/// </param>
+		public IDisposable Enter(CancellationToken cancellationToken = default(CancellationToken))
+		{
+			return mAsyncLock.Lock(cancellationToken);
+		}
+
+		/// <summary>
+		/// Asynchronously waits for a pulse signal on this monitor.
+		/// The monitor MUST already be entered when calling this method, and it will still be entered when this method returns, even if the method is cancelled.
+		/// This method internally will leave the monitor while waiting for a notification.
+		/// </summary>
+		/// <param name="cancellationToken">The cancellation signal used to cancel this wait.</param>
+		public Task WaitAsync(CancellationToken cancellationToken = default(CancellationToken))
+		{
+			return mConditionVariable.WaitAsync(cancellationToken);
+		}
+
+		/// <summary>
+		/// Asynchronously waits for a pulse signal on this monitor.
+		/// This method may block the calling thread.
+		/// The monitor MUST already be entered when calling this method, and it will still be entered when this method returns, even if the method is cancelled.
+		/// This method internally will leave the monitor while waiting for a notification.
+		/// </summary>
+		/// <param name="cancellationToken">The cancellation signal used to cancel this wait.</param>
+		public void Wait(CancellationToken cancellationToken = default(CancellationToken))
+		{
+			mConditionVariable.Wait(cancellationToken);
+		}
+
+		/// <summary>
+		/// Sends a signal to a single task waiting on this monitor.
+		/// The monitor MUST already be entered when calling this method, and it will still be entered when this method returns.
+		/// </summary>
+		public void Pulse()
+		{
+			mConditionVariable.Notify();
+		}
+
+		/// <summary>
+		/// Sends a signal to all tasks waiting on this monitor.
+		/// The monitor MUST already be entered when calling this method, and it will still be entered when this method returns.
+		/// </summary>
+		public void PulseAll()
+		{
+			mConditionVariable.NotifyAll();
+		}
+	}
+}
