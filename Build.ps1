2568d8e366529ef7f2f708038d05fe31463f460f
Sascha Falk <sascha@falk-online.eu>
2021-02-08 14:48:42 +0100

    Integrate Griffin+ build system

---
+++ Build.ps1
@@ -0,0 +1,58 @@
+[String]$ScriptDir = Split-Path $Script:MyInvocation.MyCommand.Path
+$ErrorActionPreference = "Stop"
+
+Import-Module "$ScriptDir\build.vs\GitVersion.psm1"
+Import-Module "$ScriptDir\build.vs\Clean.psm1"
+Import-Module "$ScriptDir\build.vs\RestoreNuGet.psm1"
+Import-Module "$ScriptDir\build.vs\PreBuildWizard.psm1"
+Import-Module "$ScriptDir\build.vs\Build.psm1"
+Import-Module "$ScriptDir\build.vs\PackNuGet.psm1"
+Import-Module "$ScriptDir\build.vs\PublishNuGet.psm1"
+
+# -------------------------------------------------------------------------------------------------------------------------------------
+
+# #################################
+# configuration
+# #################################
+
+[String]  $SolutionPath = "$ScriptDir\Common.sln"
+
+$MsbuildConfigurations = @('Debug','Release')
+$MsbuildPlatforms = @('Any CPU')
+
+# -------------------------------------------------------------------------------------------------------------------------------------
+
+# remove old build output
+Clean `
+	-OutputPaths @("$ScriptDir\_build", "$ScriptDir\_deploy") `
+	-PauseOnError
+
+# let GitVersion determine the version number from the git history via tags
+GitVersion -PauseOnError
+
+# restore NuGet packages
+RestoreNuGet `
+	-SolutionPath "$SolutionPath" `
+	-PauseOnError
+
+# patch templates and assembly infos with current version number
+# check consistency of NuGet packages
+PreBuildWizard `
+	-SolutionPath "$SolutionPath" `
+	-PauseOnError
+
+# build projects
+Build `
+	-SolutionPath "$SolutionPath" `
+	-MsbuildConfigurations $MsbuildConfigurations `
+	-MsbuildPlatforms $MsbuildPlatforms `
+	-SkipConsistencyCheck `
+	-PauseOnError
+
+# pack nuget packages
+PackNuGet -PauseOnError
+
+# publish nuget packages to developer feed
+PublishNuGetToDeveloperFeed -PauseOnError
+
+Read-Host "Press ANY key..."
--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,13 +2,4 @@
 
 [assembly: AssemblyCompany("Griffin+")]
 [assembly: AssemblyProduct("Griffin+ Common")]
-[assembly: AssemblyCopyright("Copyright (c) 2020 Sascha Falk and Contributors")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("--GENERATED--")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
+[assembly: AssemblyCopyright("Copyright (c) 2021 Sascha Falk and Contributors")]
