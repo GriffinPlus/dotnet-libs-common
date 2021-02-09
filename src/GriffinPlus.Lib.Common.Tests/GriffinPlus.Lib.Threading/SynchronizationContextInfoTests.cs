450a2b0bc04336dc22058aa312271887c70c53cd
Sascha Falk <sascha@falk-online.eu>
2021-02-09 08:54:28 +0100

    Add SynchronizationContextInfo class
    This class provides information about the serialization behavior of synchronization contexts.
    The synchronization contexts of Windows Forms and WPF are already defined as they are using a single thread with a message pump.
    Other synchronization contexts can be registered on demand.

---
+++ src/GriffinPlus.Lib.Common.Tests/GriffinPlus.Lib.Threading/SynchronizationContextInfoTests.cs
@@ -0,0 +1,100 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file is part of the Griffin+ common library suite (https://github.com/griffinplus/dotnet-libs-common)
+// The source code is licensed under the MIT license.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#if NETFRAMEWORK
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+using Xunit;
+
+namespace GriffinPlus.Lib.Threading
+{
+
+	/// <summary>
+	/// Unit tests targeting the <see cref="SynchronizationContextInfo"/> class.
+	/// </summary>
+	public class SynchronizationContextInfoTests
+	{
+		#region RegisterSerializingContext()
+
+		private class TestSynchronizationContext1 : SynchronizationContext
+		{
+		}
+
+		[Fact]
+		public void RegisterSerializingContext_GenericParameter()
+		{
+			var context = new TestSynchronizationContext1();
+			Assert.False(SynchronizationContextInfo.IsSerializingSynchronizationContext(context));
+			SynchronizationContextInfo.RegisterSerializingContext<TestSynchronizationContext1>();
+			Assert.True(SynchronizationContextInfo.IsSerializingSynchronizationContext(context));
+		}
+
+		private class TestSynchronizationContext2 : SynchronizationContext
+		{
+		}
+
+		[Fact]
+		public void RegisterSerializingContext_TypeParameter()
+		{
+			var context = new TestSynchronizationContext2();
+			Assert.False(SynchronizationContextInfo.IsSerializingSynchronizationContext(context));
+			SynchronizationContextInfo.RegisterSerializingContext(typeof(TestSynchronizationContext2));
+			Assert.True(SynchronizationContextInfo.IsSerializingSynchronizationContext(context));
+		}
+
+		#endregion
+
+		#region IsSerializingSynchronizationContext()
+
+		public static IEnumerable<object[]> IsSerializingSynchronizationContextTestData_Predefined
+		{
+			get
+			{
+				yield return new object[]
+				{
+					new WindowsFormsSynchronizationContext()
+				};
+
+				yield return new object[]
+				{
+					new DispatcherSynchronizationContext()
+				};
+			}
+		}
+
+		[Theory]
+		[MemberData(nameof(IsSerializingSynchronizationContextTestData_Predefined))]
+		public void IsSerializingSynchronizationContext_Predefined(SynchronizationContext context)
+		{
+			Assert.True(SynchronizationContextInfo.IsSerializingSynchronizationContext(context));
+		}
+
+		public static IEnumerable<object[]> IsSerializingSynchronizationContextTestData_Unknown
+		{
+			get
+			{
+				yield return new object[]
+				{
+					new SynchronizationContext()
+				};
+			}
+		}
+
+		[Theory]
+		[MemberData(nameof(IsSerializingSynchronizationContextTestData_Unknown))]
+		public void IsSerializingSynchronizationContext_Unknown(SynchronizationContext context)
+		{
+			Assert.False(SynchronizationContextInfo.IsSerializingSynchronizationContext(context));
+		}
+
+		#endregion
+	}
+
+}
+
+#endif
