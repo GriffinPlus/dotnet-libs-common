43171700d523cf99b7a86a0954bb149e903930ad
Sascha Falk <sascha@falk-online.eu>
2021-04-20 08:41:03 +0200

    Add 'Unicode' class with helpers for working with Unicode strings

---
+++ src/GriffinPlus.Lib.Common/GriffinPlus.Lib.Text/Unicode.cs
@@ -0,0 +1,28 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file is part of the Griffin+ common library suite (https://github.com/griffinplus/dotnet-libs-common)
+// The source code is licensed under the MIT license.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace GriffinPlus.Lib.Text
+{
+
+	/// <summary>
+	/// Some stuff that can become in handy when working with Unicode strings.
+	/// </summary>
+	public static class Unicode
+	{
+		/// <summary>
+		/// A string containing characters that are usually used to represent line breaks.
+		/// The string contains the following characters:
+		/// line feed (U+000A), form feed (U+000C), carriage return (U+000D), next line (U+0085), line separator (U+2028), paragraph separator (U+2029).
+		/// </summary>
+		public static readonly string NewLineCharacters =
+			"\u000A" + // line feed
+			"\u000C" + // form feed
+			"\u000D" + // carriage return
+			"\u0085" + // next line
+			"\u2028" + // line separator
+			"\u2029";  // paragraph separator
+	}
+
+}
