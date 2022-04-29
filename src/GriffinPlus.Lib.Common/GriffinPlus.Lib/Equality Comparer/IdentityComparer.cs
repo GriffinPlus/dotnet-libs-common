81e5df54ab8b1d8fe521dbee7fb97f04d03fdd94
Sascha Falk <sascha@falk-online.eu>
2022-04-29 07:34:40 +0200

    Optimize IdentityComparer class
    Using RuntimeHelpers.GetHashCode() instead of object.GetHashCode() is faster and fits better what the comparer should do. Two objects that are equal result in a different hash code now.

--- src/GriffinPlus.Lib.Common/GriffinPlus.Lib/Equality Comparer/IdentityComparer.cs
+++ src/GriffinPlus.Lib.Common/GriffinPlus.Lib/Equality Comparer/IdentityComparer.cs
@@ -3,8 +3,8 @@
 // The source code is licensed under the MIT license.
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace GriffinPlus.Lib
 {
@@ -26,21 +26,14 @@ namespace GriffinPlus.Lib
 		/// <param name="x">The first object of type <c>T</c> to compare.</param>
 		/// <param name="y">The second object of type <c>T</c> to compare.</param>
 		/// <returns>true if the specified objects are the same; otherwise, false.</returns>
-		public bool Equals(T x, T y)
-		{
-			return ReferenceEquals(x, y);
-		}
+		public bool Equals(T x, T y) => ReferenceEquals(x, y);
 
 		/// <summary>
 		/// Gets a hash code for the specified object.
 		/// </summary>
 		/// <param name="obj">The object for which a hash code is to be returned.</param>
 		/// <returns>A hash code for the specified object.</returns>
-		public int GetHashCode(T obj)
-		{
-			if (obj == null) throw new ArgumentNullException(nameof(obj));
-			return obj.GetHashCode();
-		}
+		public int GetHashCode(T obj) => RuntimeHelpers.GetHashCode(obj);
 	}
 
 }
